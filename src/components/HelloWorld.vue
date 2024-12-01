5c4f6744095b9b8fcef52c853e91e1ae1658f22b
ali alavi <ali.masoudi.alavi@gmail.com>
2024-12-01 18:52:47 +0330

    01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,17 @@
   </div>
 </template>
 <script setup>
-import { ref } from "vue"
+import { ref } from "vue";
 const message = ref("سلام بر جهان");
-function onCick () {
-  message.value = "خداحافظ "
+function onCick() {
+  message.value = "خداحافظ ";
 }
 </script>
+<style>
+p {
+  @apply bg-red-100 p-10 font-bold text-3xl text-red-500;
+}
+button {
+  @apply bg-blue-100 p-10 font-bold text-3xl text-blue-500;
+}
+</style>
