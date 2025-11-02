99d916ee8e07503d0f20b6c1023c9d46c4e50350
Ali Joder <ali.masoudi.alavi@gmail.com>
2025-11-02 17:45:49 +0330

    start of episode

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="bg-blue-600 text-white p-4 shadow-md ">
+        <div class="flex justify-between items-center max-w-6xl mx-auto">
+            <h1 class="text-2xl font-bold text-center">فروشگاه دیجیتال</h1>
+        </div>
+    </header>
+</template>
